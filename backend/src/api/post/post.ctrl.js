9b333504f578ad2a7d583083ec23367dad20b3cc
Dotsung <andy395023@gmail.com>
2019-04-29 13:52:27 +0900

    게시글 수정, 삭제

--- backend/src/api/post/post.ctrl.js
+++ backend/src/api/post/post.ctrl.js
@@ -99,3 +99,57 @@ exports.read = async (ctx) => {
     ctx.throw(500, err)
   }
 }
+
+// 특정 게시글 수정하기 (PUT) API '/api/post/update/:id'
+exports.update = async (ctx) => {
+  // 게시글 사용자 비교를 위한 user
+  const { user } = ctx.request
+  const { id } = ctx.params
+
+  ctx.request.body.updatedAt = Date.now()
+
+  try {
+    const post = await Post.findByIdAndUpdate(
+      id,
+      ctx.request.body,
+      { new: true }
+    ).exec()
+
+    if (!post) {
+      ctx.status = 404
+      return
+    }
+
+    // 접속한 사용자가 업거나 게시글 작성자와 다를 경우
+    if (!user || user._id !== post.author.toString()) {
+      ctx.status = 403
+      return
+    }
+
+    ctx.body = post
+  } catch (err) {
+    ctx.throw(500, err)
+  }
+}
+
+// 특정 게시글 삭제하기 (DELETE) API '/api/post/remove/:id'
+exports.remove = async (ctx) => {
+  // 게시글 사용자 비교를 위한 user
+  const { user } = ctx.request
+  const { id } = ctx.params
+
+  try {
+    const post = await Post.findById(id).exec()
+
+    if (!user || user._id !== post.author.toString()) {
+      ctx.status = 403
+      return
+    }
+
+    await Post.findByIdAndRemove(id).exec()
+
+    ctx.status = 204
+  } catch (err) {
+    ctx.throw(500, err)
+  }
+} 
