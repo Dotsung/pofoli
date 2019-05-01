8c29c6f00afbeefcdcd658d464b1affbf5e421b3
Dotsung <andy395023@gmail.com>
2019-05-01 17:40:13 +0900

    frontend 기본 구조

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,14 @@
+// @flow
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+            <>
+                home
+            </>
+        )
+    }
+}
+
+export default Home;
