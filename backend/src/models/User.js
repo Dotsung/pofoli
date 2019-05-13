3e3a01c78c8807f8ef8f073ee611a3fd1c2a2a49
Dotsung <andy395023@gmail.com>
2019-05-13 17:28:52 +0900

    유저 모델

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -4,6 +4,10 @@ import crypto from 'crypto'
 // JWT 토큰생성 함수
 import { generateToken } from 'jwt/jwt_token'
 
+const hash = (password) => {
+  return crypto.createHmac('sha256', process.env.SECRET_KEY).update(password).digest('hex');
+}
+
 const User = new mongoose.Schema({
   profile: {
     username: String,
@@ -12,6 +16,7 @@ const User = new mongoose.Schema({
     hearts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
     stars: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
     following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    follower: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   },
   email: String,
   social: {
@@ -35,30 +40,32 @@ const User = new mongoose.Schema({
   }
 });
 
-function hash (password) {
-    return crypto.createHmac(
-      'sha256',
-      process.env.SECRET_KEY
-    ).update(password).digest('hex')
-}
+User.statics.findByUsername = function(username){
+  // 객체에 내장되어있는 값을 사용 할 때는 객체명.키 이런식으로 쿼리하면 됩니다
+  return this.findOne({ 'profile.username': username }).exec();
+};
 
 User.statics.findByEmail = function (email) {
     return this.findOne({ email }).exec()
-  }
+}
   
-  // 사용자 등록 메소드
-  User.statics.register = function ({ username, email, password }) {
-    const user = new this({
-      username, email,
-      password: hash(password)
-    })
-    return user.save()
-  }
-  
+// 사용자 등록 메소드
+Account.statics.localRegister = function({ username, email, password }) {
+    // 데이터를 생성 할 때는 new this() 를 사용합니다.
+    const account = new this({
+        profile: {
+              username
+              // thumbnail 값을 설정하지 않으면 기본값으로 설정됩니다.
+          },
+          email,
+          password: hash(password)
+      });
+    return account.save();
+};
+
   // 비밀번호 비교 메소드
   User.methods.validatePassword = function (password) {
     const contrast = hash(password)
-  
     return this.password === contrast
   }
   
@@ -66,7 +73,7 @@ User.statics.findByEmail = function (email) {
   User.methods.generateToken = function () {
     const payload = {
       _id: this._id,
-      email: this.email
+      profile: this.profile
     }
     return generateToken(payload, 'User')
   }
