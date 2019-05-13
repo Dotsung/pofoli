f45279e55939a4f0a2755fe01ad6a46800438911
Dotsung <andy395023@gmail.com>
2019-05-13 20:29:38 +0900

    Article 스키마

---
+++ backend/src/models/Article.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+
+const Article = new mongoose.Schema({
+  id: Number,
+  title: String,
+  body: String,
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  image: String,
+  hearts:{ type:Number, default:0 },
+  views:{ type:Number, default:0 },
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+  stars:{ type:Number, default:0 }
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+export default mongoose.model('Article', Article) 
