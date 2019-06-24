cce15f7d7586c9cff0c9c19e1d9518e67caa1e4c
Dotsung <andy395023@gmail.com>
2019-06-24 21:12:50 +0900

    front api 수정

--- frontend/src/lib/api/post.js
+++ frontend/src/lib/api/post.js
@@ -10,9 +10,50 @@ export const write = ({ token, formData }) =>
     }
   });
 
-export const list = ({page}) => 
-  axios.get("http://localhost:4000/api/post/list/"+page);
+export const list = ({token, page}) => 
+  axios.get("http://localhost:4000/api/post/list/"+page,
+  {
+    headers: {
+      'token': token
+  }
+});
+
+export const heart = ({token, postid}) =>
+  axios.post("http://localhost:4000/api/post/heart",
+  postid,
+  {
+    headers: {
+      'token': token
+    }
+  });
+
+export const unheart = ({token, postid}) =>
+  axios.post("http://localhost:4000/api/post/unheart",
+  postid,
+  {
+    headers: {
+      'token': token
+    }
+  });
+
+export const star = ({token, postid}) =>
+  axios.post("http://localhost:4000/api/post/star",
+  postid,
+  {
+    headers: {
+      'token': token
+    }
+  });
+
+export const unstar = ({token, postid}) =>
+  axios.post("http://localhost:4000/api/post/unstar",
+  postid,
+  {
+    headers: {
+      'token': token
+    }
+  });
 
 
-  export const read = ({id}) => 
+export const read = ({id}) => 
   axios.get("http://localhost:4000/api/post/read/"+id);
