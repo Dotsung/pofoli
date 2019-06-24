3437d2804de0bbe64f49cee9226818aee3d2fc83
Dotsung <andy395023@gmail.com>
2019-06-24 09:33:12 +0900

    Heart, Star Model

--- backend/src/models/Comment.js
+++ backend/src/models/Comment.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose'
 const Comment = new mongoose.Schema({
   body: String,
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  article: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
   createdAt: {
     type: Date,
     default: Date.now
   }
 });
 
-export default mongoose.model('Comment', Comment) 
+export default mongoose.model('Comment', Comment)
