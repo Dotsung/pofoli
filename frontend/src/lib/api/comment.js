e401a7a1b99c61ed094518b0f683a3c78cda6e8a
Dotsung <andy395023@gmail.com>
2019-06-26 11:10:44 +0900

    front api 적용

---
+++ frontend/src/lib/api/comment.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const write = ({ token, body, postid }) =>
+  axios.post("http://localhost:4000/api/comment/write",
+    {
+        body: body,
+        post: postid
+    },
+    {
+      headers: {
+          'token': token,
+          'Content-Type': 'multipart/form-data'
+    }
+  });
+
+export const list = ({token}) => 
+  axios.get("http://localhost:4000/api/comment/list",
+  {
+    headers: {
+      'token': token
+  }
+});
