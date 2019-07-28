0aeb337d220481ddad79aaf451dee444ae6d0f25
haya0206 <haya0206333@gmail.com>
2019-07-28 23:03:33 +0900

    Fix #3

--- frontend/src/components/Base/WaitingModal.js
+++ frontend/src/components/Base/WaitingModal.js
@@ -1,22 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Modal = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 999;
-    background: rgba(0,0,0,0.3);
-`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 999;
+  background: rgba(0, 0, 0, 0.3);
+`;
 
-const WaitingModal = ({on}) => {
-    return (
-        <Modal>
-
-        </Modal>
-    );
+const WaitingModal = ({ on }) => {
+  return <Modal />;
 };
 
 export default WaitingModal;
--- frontend/src/containers/common/PostCardList.js
+++ frontend/src/containers/common/PostCardList.js
@@ -1,20 +1,20 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import oc from "open-color";
-import { Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import dateFormat from 'dateformat';
+import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import dateFormat from "dateformat";
 
-import { inject } from 'mobx-react';
-import { observer } from 'mobx-react-lite';
+import { inject } from "mobx-react";
+import { observer } from "mobx-react-lite";
 
 import PostCard from "components/common/PostCard";
 import PostContainer from "containers/common/PostContainer";
-import WritePostContainer from 'containers/WritePostContainer';
-import WritePostButton from 'components/common/WritePostButton';
+import WritePostContainer from "containers/WritePostContainer";
+import WritePostButton from "components/common/WritePostButton";
 
 const CardListWrapper = styled.div`
-  column-count: 6;
+  /* column-count: 6;
   width: 100%;
   padding: 10px;
   box-sizing: border-box;
@@ -34,7 +34,16 @@ const CardListWrapper = styled.div`
   }
   @media (max-width: 700px) {
     column-count: 1;
-  }
+  } */
+  width: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+  overflow-y: hidden;
+  display: grid;
+  grid-row-gap: 1px;
+  grid-column-gap: 10px;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-auto-rows: 1px;
 `;
 
 const LoadMoreButton = styled.button`
@@ -43,39 +52,47 @@ const LoadMoreButton = styled.button`
   margin: 2rem auto;
   border: 1px solid #000000;
   height: 2rem;
-`
+`;
 
 const NewPostButtonTextWrapper = styled.div`
   display: flex;
   width: 100%;
   height: 100%;
   margin: 0;
-`
+`;
 
 const NewPostButtonText = styled.p`
-    width: 250px;
-    height: 50px;
-    margin: 0 auto;
-    padding: 0;
-    display: inline-block;
-    line-height: 50px;
-    text-align: center;
-`
+  width: 250px;
+  height: 50px;
+  margin: 0 auto;
+  padding: 0;
+  display: inline-block;
+  line-height: 50px;
+  text-align: center;
+`;
 
-const PostCardList = ({postList, loadMore, getList, deleteList, getUserPostList, getHeartPostList, getStarPostList, match}) => {
-
+const PostCardList = ({
+  postList,
+  loadMore,
+  getList,
+  deleteList,
+  getUserPostList,
+  getHeartPostList,
+  getStarPostList,
+  match
+}) => {
   useEffect(() => {
     deleteList();
-    if(match){
+    if (match) {
       switch (match.params.category) {
-        case 'posts':
-          getUserPostList({username: match.params.username});
+        case "posts":
+          getUserPostList({ username: match.params.username });
           break;
-        case 'hearts':
-          getHeartPostList({username: match.params.username});
+        case "hearts":
+          getHeartPostList({ username: match.params.username });
           break;
-        case 'stars':
-          getStarPostList({username: match.params.username});
+        case "stars":
+          getStarPostList({ username: match.params.username });
           break;
         default:
           break;
@@ -83,46 +100,59 @@ const PostCardList = ({postList, loadMore, getList, deleteList, getUserPostList,
     } else {
       getList();
     }
-  },[match?match.params.category:null]);
+  }, [match ? match.params.category : null]);
 
   const NavRoute = () => {
-    if(match){
-      return <Route path={'/user/'+match.params.username+'/'+match.params.category+'/post/:postid'} component={PostContainer}/>
+    if (match) {
+      return (
+        <Route
+          path={
+            "/user/" +
+            match.params.username +
+            "/" +
+            match.params.category +
+            "/post/:postid"
+          }
+          component={PostContainer}
+        />
+      );
     }
-  }
+  };
 
+  const CardListEl = useRef(null);
   return (
     <>
-    <Route path='/post/:postid' component={PostContainer}/>
-    {NavRoute()}
-    <Route path='/write' component={WritePostContainer}/>
-    <CardListWrapper>
-      {postList.map((card, index) => (
-        <PostCard
-          id={card._id}
-          index={index}
-          title={card.title}
-          date={dateFormat(new Date(card.createdAt),"yyyy.mm.dd")}
-          author={card.author}
-          body={card.body}
-          img={card.image}
-          authorThumbnail={card.authorThumbnail}
-          authorUsername={card.authorUsername}
-          hearted={card.hearted}
-          stared={card.stared}
-          hearts={card.hearts}
-          views={card.views}
-          comments={card.comments}
-          stars={card.stars}
-          key={index}
-        />)
-      )}
-    </CardListWrapper>
-    <WritePostButton />
-    <LoadMoreButton onClick={loadMore}>Load More</LoadMoreButton>
+      <Route path="/post/:postid" component={PostContainer} />
+      {NavRoute()}
+      <Route path="/write" component={WritePostContainer} />
+      <CardListWrapper ref={CardListEl}>
+        {postList.map((card, index) => (
+          <PostCard
+            id={card._id}
+            index={index}
+            title={card.title}
+            date={dateFormat(new Date(card.createdAt), "yyyy.mm.dd")}
+            author={card.author}
+            body={card.body}
+            img={card.image}
+            authorThumbnail={card.authorThumbnail}
+            authorUsername={card.authorUsername}
+            hearted={card.hearted}
+            stared={card.stared}
+            hearts={card.hearts}
+            views={card.views}
+            comments={card.comments}
+            stars={card.stars}
+            key={index}
+            listRef={CardListEl}
+          />
+        ))}
+      </CardListWrapper>
+      <WritePostButton />
+      <LoadMoreButton onClick={loadMore}>Load More</LoadMoreButton>
     </>
   );
-}
+};
 
 export default inject(({ postListStore }) => ({
   postList: postListStore.postList,
