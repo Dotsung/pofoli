5528f6c73fa3caa32a8ce713169c048f44ee3536
Dotsung <dotsung22@gmail.com>
2019-08-03 03:58:54 +0900

    FIX #5

--- frontend/src/components/user/UserCard.js
+++ frontend/src/components/user/UserCard.js
@@ -24,19 +24,17 @@ const Wrapper = styled.div`
     box-sizing: border-box;
 `
 
-const ThumbnailWrapper = styled.div`
-`
-
 const Thumbnail = styled.img`
-    display: block;
-    width: 4rem;
-    height: 4rem;
+    width: 4.5rem;
+    height: 4.5rem;
     border-radius: 50%;
 `
 
 const Info = styled.div`
+    margin-left: 0.5rem;
     display: flex;
     flex-direction: column;
+    min-width: 0px;
 `
 
 const Username = styled.h3`
@@ -47,8 +45,10 @@ const Username = styled.h3`
 `
 
 const Intro = styled.span`
+    display: -webkit-box;
     margin: 0;
-    white-space: nowrap;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
     overflow: hidden;
     text-overflow: ellipsis;
 `
@@ -58,9 +58,7 @@ const UserCard = () => {
         <Padding>
             <WhiteBox>
                 <Wrapper>
-                    <ThumbnailWrapper>
-                        <Thumbnail src="https://dotia-files.s3.ap-northeast-2.amazonaws.com/5d10d4b61cafd92c8c6b641e49e1fda64733cc87b30df17172a07c6c.gif"/>
-                    </ThumbnailWrapper>
+                    <Thumbnail src="https://dotia-files.s3.ap-northeast-2.amazonaws.com/5d10d4b61cafd92c8c6b641e49e1fda64733cc87b30df17172a07c6c.gif"/>
                     <Info>
                         <Username>Dotsung</Username>
                         <Intro>HI, I'm dotsung.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse c</Intro>
